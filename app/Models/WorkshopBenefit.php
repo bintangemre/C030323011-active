bb5d05613b881568bd1281778a7032a51d021339
Muhammad Zidan Helmy <mzidanhelmyy@gmail.com>
2024-10-03 11:13:48 +0800

    first commit

---
+++ app/Models/WorkshopBenefit.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WorkshopBenefit extends Model
+{
+    use HasFactory;
+}
