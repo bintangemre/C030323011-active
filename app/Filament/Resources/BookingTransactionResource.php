6f5cb0e394de2b44685ef209b91dd35bc9eaf138
Muhammad Zidan Helmy <mzidanhelmyy@gmail.com>
2024-10-24 13:45:56 +0800

    view create  booking transaction

--- app/Filament/Resources/BookingTransactionResource.php
+++ app/Filament/Resources/BookingTransactionResource.php
@@ -2,16 +2,19 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\BookingTransactionResource\Pages;
-use App\Filament\Resources\BookingTransactionResource\RelationManagers;
-use App\Models\BookingTransaction;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use App\Models\Workshop;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use App\Models\BookingTransaction;
+use Filament\Forms\Components\Repeater;
+use Filament\Infolists\Components\Grid;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\BookingTransactionResource\Pages;
+use App\Filament\Resources\BookingTransactionResource\RelationManagers;
 
 class BookingTransactionResource extends Resource
 {
@@ -24,6 +27,85 @@ class BookingTransactionResource extends Resource
         return $form
             ->schema([
                 //
+                Forms\Components\Wizard::make([
+                    Forms\Components\Wizard\Step::make('Product and Price')
+                    ->schema([
+                        Forms\Components\Select::make('workshop_id')
+                        ->relationship( 'workshop','name' )
+                        ->searchable()
+                        ->preload()
+                        ->required()
+                        ->live()
+                        ->afterStateUpdated(Function ($state, callable $set){
+                            $workshop = Workshop::find($state);
+                            $set('price', $workshop ? $workshop->price : 0);
+                        })
+                        ->afterStateUpdated(Function ($state, callable $get, callable $set){
+                            $workshop = Workshop::find($state);
+                            $set('price', $workshop ? $workshop->price : 0);
+                        }),
+                        Forms\Components\TextInput::make('quantity')
+                        ->required()
+                        ->numeric()
+                        ->prefix('Qty People')
+                        ->live()
+                        ->afterStateUpdated(Function ($state, callable $get, callable $set){
+                            $price = $get('price');
+                            $subTotal = $price  * $state;
+                            $totalPpn = $subTotal * 0.11;
+                            $totalAmount = $subTotal + $totalPpn;
+
+                            $set('total_amount', $totalAmount);
+
+                            $participants = $get('participants', 0, $state) ?? [];
+                            $currentCount = count($participants);
+
+                            if ($state > $currentCount) {
+                                for ($i = $currentCount; $i < $state; $i++) { 
+                                    $participants[] = ['name' => '', 'occupation' => '', 'email' => ''];
+                                }
+                            }else {
+                                $participants =array_slice($participants, 0, $state);
+                            }
+                            $set ('participants', $participants);
+                        })
+                        ->afterStateHydrated(Function ($state, callable $get, callable $set){
+                            // calculate total amount when  the form  is loaded  with existing data
+                           $price = $get('price');
+                           $subTotal = $price  * $state;
+                           $totalPpn = $subTotal * 0.11;
+                           $totalAmount = $subTotal + $totalPpn;
+
+                           $set('total_amount', $totalAmount);
+                        }),
+                        Forms\Components\TextInput::make('total_amount' )
+                        ->required()
+                        ->numeric()
+                        ->prefix('IDR')
+                        ->readOnly()
+                        ->helperText('Harga sudah include PPN 11%'),
+
+                        Repeater::make( 'Participants' )
+                        ->schema([
+                            Grid::make(2)
+                            ->schema([
+                                Forms\Components\TextInput::make('name')
+                                ->label('Participant Name')
+                                ->required(),
+
+                                Forms\Components\TextInput::make('occupation')
+                                ->label('Occupation')
+                                ->required(),
+
+                                Forms\Components\TextInput::make('email')
+                                ->label('Email')
+                                ->required(),
+                            ]),
+                        ])
+                        ->column(1)
+                        ->label('Participant Detail')    
+                        ]),
+                ])
             ]);
     }
 
