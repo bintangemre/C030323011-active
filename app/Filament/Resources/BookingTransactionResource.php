daae698139e5b122866ab7da572ee74b4c4e03fc
Muhammad Zidan Helmy <mzidanhelmyy@gmail.com>
2024-10-26 13:07:58 +0800

    Step 3 booking transaction

--- app/Filament/Resources/BookingTransactionResource.php
+++ app/Filament/Resources/BookingTransactionResource.php
@@ -10,6 +10,7 @@ use Filament\Forms\Components\Repeater;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Forms\Components\ToggleButtons;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -29,83 +30,133 @@ class BookingTransactionResource extends Resource
                 //
                 Forms\Components\Wizard::make([
                     Forms\Components\Wizard\Step::make('Product and Price')
-                    ->schema([
-                        Forms\Components\Select::make('workshop_id')
-                        ->relationship( 'workshop','name' )
-                        ->searchable()
-                        ->preload()
-                        ->required()
-                        ->live()
-                        ->afterStateUpdated(Function ($state, callable $set){
-                            $workshop = Workshop::find($state);
-                            $set('price', $workshop ? $workshop->price : 0);
-                        })
-                        ->afterStateUpdated(Function ($state, callable $get, callable $set){
-                            $workshop = Workshop::find($state);
-                            $set('price', $workshop ? $workshop->price : 0);
-                        }),
-                        Forms\Components\TextInput::make('quantity')
-                        ->required()
-                        ->numeric()
-                        ->prefix('Qty People')
-                        ->live()
-                        ->afterStateUpdated(Function ($state, callable $get, callable $set){
-                            $price = $get('price');
-                            $subTotal = $price  * $state;
-                            $totalPpn = $subTotal * 0.11;
-                            $totalAmount = $subTotal + $totalPpn;
-
-                            $set('total_amount', $totalAmount);
-
-                            $participants = $get('participants', 0, $state) ?? [];
-                            $currentCount = count($participants);
-
-                            if ($state > $currentCount) {
-                                for ($i = $currentCount; $i < $state; $i++) { 
-                                    $participants[] = ['name' => '', 'occupation' => '', 'email' => ''];
-                                }
-                            }else {
-                                $participants =array_slice($participants, 0, $state);
-                            }
-                            $set ('participants', $participants);
-                        })
-                        ->afterStateHydrated(Function ($state, callable $get, callable $set){
-                            // calculate total amount when  the form  is loaded  with existing data
-                           $price = $get('price');
-                           $subTotal = $price  * $state;
-                           $totalPpn = $subTotal * 0.11;
-                           $totalAmount = $subTotal + $totalPpn;
-
-                           $set('total_amount', $totalAmount);
-                        }),
-                        Forms\Components\TextInput::make('total_amount' )
-                        ->required()
-                        ->numeric()
-                        ->prefix('IDR')
-                        ->readOnly()
-                        ->helperText('Harga sudah include PPN 11%'),
-
-                        Forms\Components\Repeater::make( 'Participants' )
                         ->schema([
-                            Forms\Components\Grid::make(2)
-                            ->schema([
-                                Forms\Components\TextInput::make('name')
-                                ->label('Participant Name')
-                                ->required(),
+                            Forms\Components\Select::make('workshop_id')
+                                ->relationship('workshop', 'name')
+                                ->searchable()
+                                ->preload()
+                                ->required()
+                                ->live()
+                                ->afterStateUpdated(function ($state, callable $set) {
+                                    $workshop = Workshop::find($state);
+                                    $set('price', $workshop ? $workshop->price : 0);
+                                })
+                                ->afterStateUpdated(function ($state, callable $get, callable $set) {
+                                    $workshop = Workshop::find($state);
+                                    $set('price', $workshop ? $workshop->price : 0);
+                                }),
+                            Forms\Components\TextInput::make('quantity')
+                                ->required()
+                                ->numeric()
+                                ->prefix('Qty People')
+                                ->live()
+                                ->afterStateUpdated(function ($state, callable $get, callable $set) {
+                                    $price = $get('price');
+                                    $subTotal = $price  * $state;
+                                    $totalPpn = $subTotal * 0.11;
+                                    $totalAmount = $subTotal + $totalPpn;
 
-                                Forms\Components\TextInput::make('occupation')
-                                ->label('Occupation')
-                                ->required(),
+                                    $set('total_amount', $totalAmount);
 
-                                Forms\Components\TextInput::make('email')
-                                ->label('Email')
-                                ->required(),
-                            ]),
-                        ])
-                        // ->column(1)
-                        ->label('Participant Detail')    
+                                    $participants = $get('participants', 0, $state) ?? [];
+                                    $currentCount = count($participants);
+
+                                    if ($state > $currentCount) {
+                                        for ($i = $currentCount; $i < $state; $i++) {
+                                            $participants[] = ['name' => '', 'occupation' => '', 'email' => ''];
+                                        }
+                                    } else {
+                                        $participants = array_slice($participants, 0, $state);
+                                    }
+                                    $set('participants', $participants);
+                                })
+                                ->afterStateHydrated(function ($state, callable $get, callable $set) {
+                                    // calculate total amount when  the form  is loaded  with existing data
+                                    $price = $get('price');
+                                    $subTotal = $price  * $state;
+                                    $totalPpn = $subTotal * 0.11;
+                                    $totalAmount = $subTotal + $totalPpn;
+
+                                    $set('total_amount', $totalAmount);
+                                }),
+                            Forms\Components\TextInput::make('total_amount')
+                                ->required()
+                                ->numeric()
+                                ->prefix('IDR')
+                                ->readOnly()
+                                ->helperText('Harga sudah include PPN 11%'),
+
+                            Repeater::make('Participants')
+                                ->schema([
+                                    Grid::make(2)
+                                        ->schema([
+                                            Forms\Components\TextInput::make('name')
+                                                ->label('Participant Name')
+                                                ->required(),
+
+                                            Forms\Components\TextInput::make('occupation')
+                                                ->label('Occupation')
+                                                ->required(),
+
+                                            Forms\Components\TextInput::make('email')
+                                                ->label('Email')
+                                                ->required(),
+                                        ]),
+                                ])
+                                ->columns(1)
+                                ->label('Participant Detail')
                         ]),
+                    Forms\Components\Wizard\Step::make('Costumer information')
+                        ->schema([
+                            Forms\Components\TextInput::make('name')
+                                ->required()
+                                ->maxLength(255),
+
+                            Forms\Components\TextInput::make('email')
+                                ->required()
+                                ->maxLength(255),
+
+                            Forms\Components\TextInput::make('phone')
+                                ->required()
+                                ->maxLength(255),
+
+                            Forms\Components\TextInput::make('custumer_bank_name')
+                                ->required()
+                                ->maxLength(255),
+
+                            Forms\Components\TextInput::make('customer_bank_account')
+                                ->required()
+                                ->maxLength(255),
+
+                            Forms\Components\TextInput::make('customer_bank_number')
+                                ->required()
+                                ->maxLength(255),
+
+                            Forms\Components\TextInput::make('booking_trx_id')
+                                ->required()
+                                ->maxLength(255),
+                        ]),
+                        Forms\Components\Wizard\Step::make('Payment information')
+                        ->schema([
+                            ToggleButtons::make('is_paid')
+                            ->label('Apakah sudah membayar?')
+                            ->boolean()
+                            ->grouped()
+                            ->icons([
+                                true => 'heroicon-o-pencil',
+                                false => 'heroicon-o-clock',
+                            ])
+                            ->required(),
+
+                            Forms\Components\FileUpload::make('proof')
+                            ->label('Upload Bukti Pembayaran')
+                            ->image()
+                            ->required(),
+                            ]),
                 ])
+                ->columnSpan('full') //use full width for the wizard
+                ->columns(1)
+                ->skippable()
             ]);
     }
 
