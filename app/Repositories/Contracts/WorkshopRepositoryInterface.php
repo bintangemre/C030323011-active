39e2c2d5e072fa3917b16a9733525238a3e38fe8
Muhammad Zidan Helmy <mzidanhelmyy@gmail.com>
2024-11-07 11:57:12 +0800

    2 file repositories workshop

---
+++ app/Repositories/Contracts/WorkshopRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+interface WorkshopRepositoryInterface
+
+{
+    public function getAllNewWorkshops();
+
+    public function find($id);
+
+    public function getPrice($workshopId);
+}
